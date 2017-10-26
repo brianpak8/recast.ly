1d6a18347582bc4e701c7d702f7fde8e89139386
Hack Reactor Student <student@hackreactor.com>
2017-10-26 12:26:17 -0700

    Start implementing search function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,4 +1,23 @@
 var searchYouTube = (options, callback) => {
+  $.ajax({
+    
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    method: 'GET',
+    data: {
+      maxResults: options.max,
+      key: options.key,
+      q: options.query,
+      type: options.type
+    },
+    done: function () {
+      console.log('success');
+    },
+    fail: function() {
+      console.log('failure');
+    }
+    
+    
+  });
   // TODO
 };
 
