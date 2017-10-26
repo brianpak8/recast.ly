8614f1fea59d2458b6f246d29d9e8ca7a28602f4
Hack Reactor Student <student@hackreactor.com>
2017-10-25 21:03:32 -0700

    end of night, working on app bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentVideo: this.props.videos[0] };
+    this.state = { currentVideo: '' };
     this.handleClick = this.handleClick.bind(this);
   }
   
@@ -15,7 +15,7 @@ class App extends React.Component {
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayer video={this.props.videos[0]}/>
           </div>
           <div className="col-md-5">
             <VideoList videos={this.props.videos} handleClick = {this.handleClick}/>
